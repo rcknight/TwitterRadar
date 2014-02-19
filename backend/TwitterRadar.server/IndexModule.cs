9cc510ecde9a5fb65ed5dd9a3bc8def8121aaa1f
Rich Knight <rich@rich-knight.com>
2014-02-19 17:03:12 +0000

    initial commit

---
+++ backend/TwitterRadar.server/IndexModule.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.Linq;
+using TweetSharp;
+using Nancy;
+
+namespace TwitterRadar.Server
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/location"] = (paramaters) =>
+                {
+                    var service = new TwitterService(ConfigurationManager.AppSettings["TWITTER_CONSUMER_KEY"], ConfigurationManager.AppSettings["TWITTER_CONSUMER_SECRET"]);
+                    service.AuthenticateWith(ConfigurationManager.AppSettings["TWITTER_ACCESS_TOKEN"],ConfigurationManager.AppSettings["TWITTER_ACCESS_TOKEN_SECRET"]);
+
+                    var tweets = service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions()
+                        {
+                            ContributorDetails = true,
+                            Count = 1,
+                            ExcludeReplies = false,
+                            IncludeRts = true,
+                            ScreenName = "jen20"
+                        });
+
+                    
+
+                    return Response.AsJson(new
+                        {
+                            LastTweet = tweets.First().TextAsHtml,
+                            Location = tweets.First().Location,
+                            Date =
+                                tweets.First().CreatedDate.ToShortDateString() + " " +
+                                tweets.First().CreatedDate.ToShortTimeString()
+                        }).WithHeaders(new []
+                            {
+                                new { Header = "Access-Control-Allow-Origin", Value="*" }
+                            });
+                };
+        }
+    }
+}
